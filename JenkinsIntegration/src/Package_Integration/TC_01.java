49973ac206d3f19827153d90be082a4af8227bd2
Jyotsa <jyotsnasingh@192.168.29.96>
2022-01-20 19:54:51 +0530

    Integration suite

---
+++ JenkinsIntegration/src/Package_Integration/TC_01.java
@@ -0,0 +1,18 @@
+package Package_Integration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+@Test
+public class TC_01 {
+	public void test_01() throws InterruptedException
+	{
+		System.setProperty("webdriver.chrome.driver", "//Users//jyotsnasingh//Downloads//chromedriver");
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://testsite.com");
+		//driver.wait(1000);
+		driver.quit();
+	}
+
+}
